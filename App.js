b7b89459ba902dfc71fbc269519234db6c88a3f7
patrick.sartori <patrick.sartori@supsi.ch>
2023-03-02 16:01:15 +0100

    Code to grab accelerometer data

--- App.js
+++ App.js
@@ -5,59 +5,52 @@ import { Accelerometer, Gyroscope } from 'expo-sensors';
 
 
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
-  const [subscription, setSubscription] = useState(null);
-
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
-
-  const _subscribe = () => {
-    setSubscription(
-      Accelerometer.addListener(setData)
-    );
-  };
-
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
-
+  const [data, setData] = useState({});
+  
   useEffect(() => {
     _subscribe();
-    return () => _unsubscribe();
   }, []);
 
+  const _subscribe = () => {
+    this._subscription = Accelerometer.addListener(accelerometerData => {
+      setData(accelerometerData);
+    });
+  };
+  
+  let { x, y, z } = data;
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
+      <Text style={styles.title}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+      <Text>
+        x: {x}
+      </Text>
+      <Text>
+        y: {y}
+      </Text>
+      <Text>
+        z: {z}
+      </Text>
     </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
