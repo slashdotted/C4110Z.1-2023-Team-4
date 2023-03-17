2366b0eb599e9d49df1454b0214614a5c7ac4c91
patrick.sartori <patrick.sartori@supsi.ch>
2023-03-17 11:01:07 +0100

    Adding buttons with navigation functions

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 import { Accelerometer, Gyroscope } from 'expo-sensors';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Start"
+        onPress={() =>
+          navigation.navigate('Resort', { name: 'Resort' })
+        }
+      />
+      <Button
+        title="Emergency Numbers"
+        onPress={() =>
+          navigation.navigate('EmergencyNumbers', { name: 'Emergency Numbers' })
+        }
+      />
+      <Button
+        title="Resorts"
+        onPress={() =>
+          navigation.navigate('Resort', { name: 'Resort' })
+        }
+      />
+    </View>
+  );
+}
+
+function ResortScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Resort 1"
+      />
+      <Button
+        title="Resort 2"
+      />
+      <Button
+        title="Resort 3"
+      />
+    </View>
+  );
+}
+
+function EmergencyNumbersScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>TODO: ADD EMERGENCY NUMBERS</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 
-
+/*
 export default function App() {
   const [data, setData] = useState({});
   
@@ -26,17 +79,46 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.topText}>
-        <Text style={styles.innerTopText}>SKI ACCIDENT DETECTOR</Text>
+      <View style={styles.topContainer}>
+        <Text>SKI ACCIDENT DETECTOR</Text>
       </View>
       <TouchableOpacity
         style={styles.roundButton}>
         <Text style={styles.whiteText}>START</Text>
       </TouchableOpacity>
+      <View style={styles.topContainer}>
+        <Text>SKI ACCIDENT DETECTOR</Text>
+      </View>
     </View>
   );
 };
+*/
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'SKI ACCIDENT DETECTION' }}
+        />
+        <Stack.Screen
+          name="Resort"
+          component={ResortScreen}
+          options={({ route }) => ({ title: route.params.name })}
+        />
+        <Stack.Screen
+          name="EmergencyNumbers"
+          component={EmergencyNumbersScreen}
+          options={({ route }) => ({ title: route.params.name })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
+export default App;
+/* 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -60,14 +142,14 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 48,
   },
-  topText:{
-    borderBottomColor:'black',
-    borderBottomWidth:1,
-    height:'12%',
-    width:'150%',
-    backgroundColor:'#fff'
+  topContainer: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    alignContent:'center',
+    height:'10%',
+    width:'100%',
+    color:'black',
+    fontSize:18,
+    marginTop:0
   },
-  innerTopText:{
-    fontSize: 18,
-  }
-});
+});*/
